303fb3172935ad70638d0f213ad88ef310f19bb0
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-08-23 20:44:40 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,13 +1,10 @@
 <?php
-// app/Http/Controllers/AuthController.php
 
 namespace App\Http\Controllers;
 
 use App\Models\AdminUser;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -16,9 +13,6 @@ class AuthController extends Controller
         $request->validate([
             'username' => 'required|string',
             'password' => 'required|string',
-        ], [
-            'username.required' => 'Username wajib diisi',
-            'password.required' => 'Password wajib diisi',
         ]);
 
         $user = AdminUser::where('username', $request->username)->first();
@@ -45,16 +39,11 @@ class AuthController extends Controller
         $request->validate([
             'username' => 'required|string|min:3|unique:admin_users,username',
             'password' => 'required|string|min:6',
-        ], [
-            'username.required' => 'Username wajib diisi',
-            'username.unique' => 'Username sudah digunakan',
-            'password.required' => 'Password wajib diisi',
-            'password.min' => 'Password minimal 6 karakter',
         ]);
 
         $user = AdminUser::create([
             'username' => $request->username,
-            'password' => $request->password, // Will be hashed by mutator
+            'password' => Hash::make($request->password), // penting! 
         ]);
 
         return response()->json([
