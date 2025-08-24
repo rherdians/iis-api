48c8913d04f64f0945efdfc6b2e085f7e38c57e2
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-08-24 10:52:44 +0700

    feat: add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,8 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\Cors::class, // Middleware CORS Anda
+        \App\Http\Middleware\CorsMiddleware::class,
+
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin'      => 'https://islamic-it-school.com',
+            'Access-Control-Allow-Methods'     => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, X-CSRF-Token',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+    }
+}
